310dd2f8bd59af0db982ae20196616252e17a010
Fidelis <k77705@protonmail.com>
2024-08-20 11:18:07 +1000

    add model&pic resources

--- models/resnet/resnet_generator.py
+++ models/resnet/resnet_generator.py
@@ -1,10 +1,9 @@
 import torch
-import torchvision.models as models
 import pnnx
 
 
 #resnet
-net = models.resnet18(pretrained=True)
+model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=True)
 x = torch.rand(1, 3, 224, 224)
 
-opt_net = pnnx.export(net, "resnet18.pt", x)
+opt_net = pnnx.export(model, "resnet18.pt", x)
