5cdee3c506b596877945dd51766312e8d8a22544
Fidelis <k77705@protonmail.com>
2024-08-23 13:20:03 +1000

    add models

--- models/unet/unet_generator.py
+++ models/unet/unet_generator.py
@@ -5,3 +5,7 @@ import pnnx
 
 #unet
 model = torch.hub.load('mateuszbuda/brain-segmentation-pytorch', 'unet', in_channels=3, out_channels=1, init_features=32, pretrained=True)
+
+x = torch.rand(1, 3, 256, 256)
+
+opt_net = pnnx.export(model, "unet.pt", x)
